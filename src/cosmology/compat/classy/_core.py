f41e714a971977c8284006de9bfa08ce1199e2b2
Nathaniel Starkman <nstarman@users.noreply.github.com>
2023-05-15 22:34:17 -0400

    Make `__cosmology_namespace__` a property (#10)
    Signed-off-by: Nathaniel Starkman <nstarman@users.noreply.github.com>

--- src/cosmology/compat/classy/_core.py
+++ src/cosmology/compat/classy/_core.py
@@ -43,28 +43,9 @@ class CosmologyWrapper(CosmologyWrapperAPI[Array, InputT]):
         self._cosmo_fn: dict[str, Any]
         object.__setattr__(self, "_cosmo_fn", {})
 
-    def __cosmology_namespace__(
-        self, /, *, api_version: str | None = None
-    ) -> CosmologyNamespace:
-        """Returns an object that has all the cosmology API functions on it.
-
-        Parameters
-        ----------
-        api_version: Optional[str]
-            string representing the version of the cosmology API specification
-            to be returned, in ``'YYYY.MM'`` form, for example, ``'2020.10'``.
-            If ``None``, it return the namespace corresponding to latest version
-            of the cosmology API specification.  If the given version is invalid
-            or not implemented for the given module, an error is raised.
-            Default: ``None``.
-
-            .. note:: currently only `None` is supported.
-
-        Returns
-        -------
-        `CosmologyNamespace`
-            An object representing the CAMB cosmology API namespace.
-        """
+    @property
+    def __cosmology_namespace__(self) -> CosmologyNamespace:
+        """Returns :class:`~cosmology.api.CosmologyNamespace` for CLASS."""
         import cosmology.compat.classy as namespace
 
         return cast(CosmologyNamespace, namespace)
